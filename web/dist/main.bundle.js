cfc57ab456367c86525176d3f5e82e6337a9af6b
John Boyer <joeRob2468@yahoo.com>
2020-04-06 15:09:15 -0500

    small changes to setup

--- web/dist/main.bundle.js
+++ web/dist/main.bundle.js
@@ -401,13 +401,13 @@ $(function(){var H=new _dogstudio_highway__WEBPACK_IMPORTED_MODULE_2___default.a
   !*** ./src/scss/style.scss ***!
   \*****************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-// extracted by mini-css-extract-plugin
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/lib/loader.js):\nError: Node Sass does not yet support your current environment: Linux 64-bit with Unsupported runtime (79)\nFor more information on which environments are supported please see:\nhttps://github.com/sass/node-sass/releases/tag/v4.12.0\n    at module.exports (/var/www/node_modules/node-sass/lib/binding.js:13:13)\n    at Object.<anonymous> (/var/www/node_modules/node-sass/lib/index.js:14:35)\n    at Module._compile (/var/www/node_modules/v8-compile-cache/v8-compile-cache.js:192:30)\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1167:10)\n    at Module.load (internal/modules/cjs/loader.js:996:32)\n    at Function.Module._load (internal/modules/cjs/loader.js:896:14)\n    at Module.require (internal/modules/cjs/loader.js:1036:19)\n    at require (/var/www/node_modules/v8-compile-cache/v8-compile-cache.js:161:20)\n    at getDefaultSassImpl (/var/www/node_modules/sass-loader/lib/loader.js:203:10)\n    at Object.sassLoader (/var/www/node_modules/sass-loader/lib/loader.js:79:31)\n    at /var/www/node_modules/webpack/lib/NormalModule.js:313:20\n    at /var/www/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /var/www/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at runSyncOrAsync (/var/www/node_modules/loader-runner/lib/LoaderRunner.js:143:3)\n    at iterateNormalLoaders (/var/www/node_modules/loader-runner/lib/LoaderRunner.js:232:2)\n    at Array.<anonymous> (/var/www/node_modules/loader-runner/lib/LoaderRunner.js:205:4)\n    at Storage.finished (/var/www/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:43:16)\n    at /var/www/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:79:9\n    at /var/www/node_modules/graceful-fs/graceful-fs.js:90:16\n    at FSReqCallback.readFileAfterClose [as oncomplete] (internal/fs/read_file_context.js:63:3)");
 
 /***/ }),
 
-/***/ 1:
+/***/ 0:
 /*!*********************!*\
   !*** got (ignored) ***!
   \*********************/
