6971bd08c4b07b8884921acf90f436cdeb5b8582
John Boyer <joeRob2468@yahoo.com>
2020-08-03 14:52:46 -0500

    added deployment scripts and copied over a bit more bootstrap stuff

---
+++ deployment-script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This shell script is tool to automate git deployment
+# through webhooks. Be sure to run from within the root directory.
+
+# Backup assets and db if env environment is set
+if [ -f "./.env.sh" ]; then
+    printf -- ' Creating backups...';
+    ./shell-scripts/backup_db.sh
+    ./shell-scripts/backup_assets.sh
+    printf -- ' DONE!\n';
+fi
+
+# merge git changes
+git merge
+
+# Run composer install
+printf -- ' Updating composer...';
+cd cms
+if composer install --no-interaction --prefer-dist --optimize-autoloader; then
+    printf -- ' DONE!\n';
+    
+    # run Craft CMS migration and project sync command
+    printf -- ' Running Craft CMS migration...';
+    php ./craft migrate/all
+    php ./craft project-config/sync
+    printf -- ' DONE!\n';
+
+    # clear caches
+    printf -- ' Clearing caches...';
+    php ./craft clear-caches/all
+    printf -- ' DONE!\n';
+fi
+
+# cd back to main directory, add additional scripts below
+cd ../
