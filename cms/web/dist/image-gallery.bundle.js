89c0e44bb9f500d6212d72df4b85d7f3154fb4dd
John Boyer <joeRob2468@yahoo.com>
2021-03-16 16:49:48 -0500

    updated team_grid content block, removed unused assets from dist

--- cms/web/dist/image-gallery.bundle.js
+++ cms/web/dist/image-gallery.bundle.js
@@ -1,20 +1,2 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["image-gallery"],{
-
-/***/ "./src/js/block-modules/ImageGallery.js":
-/*!**********************************************!*\
-  !*** ./src/js/block-modules/ImageGallery.js ***!
-  \**********************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* WEBPACK VAR INJECTION */(function($) {/* harmony import */ var swiper_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! swiper/core */ "./node_modules/swiper/swiper.esm.js");
-function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}function _createClass(Constructor,protoProps,staticProps){if(protoProps)_defineProperties(Constructor.prototype,protoProps);if(staticProps)_defineProperties(Constructor,staticProps);return Constructor;}function _defineProperty(obj,key,value){if(key in obj){Object.defineProperty(obj,key,{value:value,enumerable:true,configurable:true,writable:true});}else{obj[key]=value;}return obj;}var ImageGallery=/*#__PURE__*/function(){function ImageGallery(){_classCallCheck(this,ImageGallery);swiper_core__WEBPACK_IMPORTED_MODULE_0__["default"].use([swiper_core__WEBPACK_IMPORTED_MODULE_0__["Navigation"]]);this.sliders=[];}_createClass(ImageGallery,[{key:"init",value:function init(){var _this=this;var elements=$(ImageGallery.selector);if(elements.length){elements.each(function(index,element){var slider=new swiper_core__WEBPACK_IMPORTED_MODULE_0__["default"]($('.swiper-container',element)[0],{centeredSlides:true,loop:true,initialSlide:0,spaceBetween:15,slidesPerView:1.2,breakpoints:{1024:{spaceBetween:35,slidesPerView:1.4}}});_this.sliders.push(slider);});}}},{key:"destroy",value:function destroy(){if(this.sliders.length){// destroy slider instances
-for(var i=0;i<this.sliders.length;i++){var slider=this.sliders[i];slider.destroy();}this.sliders=[];}}}]);return ImageGallery;}();_defineProperty(ImageGallery,"selector",'.section-image_gallery--block');/* harmony default export */ __webpack_exports__["default"] = (ImageGallery);
-/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
-
-/***/ })
-
-}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{126:function(e,i,n){"use strict";n.r(i),function(e){var t=n(311),r=n(310);function s(e,i){for(var n=0;n<i.length;n++){var t=i[n];t.enumerable=t.enumerable||!1,t.configurable=!0,"value"in t&&(t.writable=!0),Object.defineProperty(e,t.key,t)}}var a,l,o,c=function(){function i(){!function(e,i){if(!(e instanceof i))throw new TypeError("Cannot call a class as a function")}(this,i),t.a.use([r.a]),this.sliders=[]}var n,a,l;return n=i,(a=[{key:"init",value:function(){var n=this,r=e(i.selector);r.length&&r.each((function(i,r){var s=new t.a(e(".swiper-container",r)[0],{centeredSlides:!0,loop:!0,initialSlide:0,spaceBetween:15,slidesPerView:1.2,breakpoints:{1024:{spaceBetween:35,slidesPerView:1.4}}});n.sliders.push(s)}))}},{key:"destroy",value:function(){if(this.sliders.length){for(var e=0;e<this.sliders.length;e++)this.sliders[e].destroy();this.sliders=[]}}}])&&s(n.prototype,a),l&&s(n,l),i}();o=".section-image_gallery--block",(l="selector")in(a=c)?Object.defineProperty(a,l,{value:o,enumerable:!0,configurable:!0,writable:!0}):a[l]=o,i.default=c}.call(this,n(16))}}]);
 //# sourceMappingURL=image-gallery.bundle.js.map
